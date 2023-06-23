a05acf1edc0d05321f5dec5376cd7bafc193c772
Agus Siswanto <agus.siswanto@erajaya.com>
2023-06-23 15:29:35 +0700

    add fullhuman/postcss-purgecss

--- web/config/postcss.config.js
+++ web/config/postcss.config.js
@@ -1,8 +1,22 @@
 const path = require('path')
 
+const purgecss = require('@fullhuman/postcss-purgecss')({
+  content: [
+    path.resolve(__dirname, '..', 'src', '**', '!(*.test).js'),
+    path.resolve(__dirname, '..', 'src', '**', '*.html'),
+    path.resolve(__dirname, '..', 'src', '**', '*.tsx'),
+    path.resolve(__dirname, '..', 'src', '**', '*.ts'),
+    path.resolve(__dirname, '..', 'src', '**', '*.jsx'),
+  ],
+  // This extractor is used for tailwind classes.
+  // Read more here: https://tailwindcss.com/docs/controlling-file-size/
+  defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+})
+
 module.exports = {
   plugins: [
     require('tailwindcss')(path.resolve(__dirname, 'tailwind.config.js')),
     require('autoprefixer'),
+    ...(process.env.NODE_ENV === 'production' ? [purgecss] : []),
   ],
 }
